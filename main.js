630b329d21b0b37644eee643584e08649015bfa7
Vyshnav K S <62277580+Vyshnav-KS@users.noreply.github.com>
2020-12-08 13:28:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+    //show
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+//hidden
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+
+//remove menu
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+//scroll selection active link
+const sections = documemnt.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionID = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href='+ sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href='+ sectionId + ']').classList.remove('active')
+        }
+    })
+}
