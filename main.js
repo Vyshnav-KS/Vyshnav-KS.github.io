985f3d56e0e052122e9275d1e363d53ab10a4e09
Vyshnav-KS <Vyshnavks85@gmail.com>
2021-03-16 21:26:49 +0530

    modifications

--- main.js
+++ main.js
@@ -1,43 +1,46 @@
-const navMenu = document.getElementById('nav-menu'),
-    toggleMenu = document.getElementById('nav-toggle'),
-    closeMenu = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+	toggleMenu = document.getElementById("nav-toggle"),
+	closeMenu = document.getElementById("nav-close");
 
-    //show
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
+//show
+toggleMenu.addEventListener("click", () => {
+	navMenu.classList.toggle("show");
+});
 
 //hidden
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
-
+closeMenu.addEventListener("click", () => {
+	navMenu.classList.remove("show");
+});
 
 //remove menu
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    navMenu.classList.remove('show')
+function linkAction() {
+	navMenu.classList.remove("show");
 }
-navLink.forEach(n => n.addEventListener('click',linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 //scroll selection active link
-const sections = documemnt.querySelectorAll('section[id]')
+const sections = documemnt.querySelectorAll("section[id]");
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+	const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionID = current.getAttribute('id')
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+		sectionID = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href='+ sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href='+ sectionId + ']').classList.remove('active')
-        }
-    })
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector(".nav__menu a[href=" + sectionId + "]")
+				.classList.add("active");
+		} else {
+			document
+				.querySelector(".nav__menu a[href=" + sectionId + "]")
+				.classList.remove("active");
+		}
+	});
 }
